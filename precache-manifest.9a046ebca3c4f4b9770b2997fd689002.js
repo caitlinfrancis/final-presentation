b16d7b30e1939bd01c970e00597c6970ec10985b
unknown <caitlinfrancis2424@gmail.com>
2019-12-07 21:23:46 -0600

    Updates

---
+++ precache-manifest.9a046ebca3c4f4b9770b2997fd689002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8750bbb26dfadb82bc601ea33939fa90",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a8fa06f4288b5dd2d27f",
+    "url": "/monsters-rolodex/static/css/main.63bcfa3c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8fa06f4288b5dd2d27f",
+    "url": "/monsters-rolodex/static/js/main.41c328ee.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
